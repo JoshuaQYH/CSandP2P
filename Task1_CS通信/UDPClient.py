da514d80c898b260a5b579c63155838deef64120
JoshuaQYh <576261090@qq.com>
2018-10-11 07:37:25 +0800

    download

--- Task1_CS通信/UDPClient.py
+++ Task1_CS通信/UDPClient.py
@@ -1,44 +1,106 @@
 import socket
+import sys
 
-savedFileName = "saveData.txt"
+####################################### 变量初始化
+# 服务端下载的文件的文件名
+savedFileName = ""
 
-def sockClient():
-    serverAddress = ('127.0.0.1', 31500)
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    clientAddress = ('127.0.0.1', 31501)
-    s.bind(clientAddress)
+#######################################  加密解密函数
+
+
+########################################  socket配置相关
+# 服务端地址
+serverAddress = ('127.0.0.1', 31500)
+# 创建客户端socket 
+mainSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+clientAddress = ('127.0.0.1', 31501)
+# 绑定客户端 端口
+mainSocket.bind(clientAddress)
+
+
+####################################### socket相关功能函数
+# 发送请求给服务器，请求服务器返回文件资源列表
+def listFile():
+    print("Ready to list the files on Server.")
+
+    try:
+        clientData, clientAddr = mainSocket.recvfrom(51200)
+    except ConnectionResetError:
+        print("[Error]: Port is wrong")
+    except:
+        print("Timeout or some other errors")
+    
+    fileList = clientData.decode('utf-8')
+    print(fileList)
+
+    print("All Files on server are here.")
+    return 0
+
+def judgeFileName(fileName):
+    return False;
+
+# 发送下载命令，请求服务端返回该文件并接收保存，接收服务端消息提示
+def downloadFile(fileName):
+    print("Ready to download file from server")
+    # 发送请求的文件名，此处假设文件名正确
+    mainSocket.sendto(fileName, serverAddress)
+
     while True:
-        # 发送请求，传给服务器
-        msg = input("Input the command:")
-        
-        if not msg:
-            break
-
-        s.sendto(msg.encode(), serverAddress)
-        
-        # 接受服务器的消息
-        data, addr = s.recvfrom(2048)
-        if not data:
-            print("no data from server")
-            continue
-
-        # 捕获接收文件的信号 START
-        if data == b'START':
-            # 打开文件，开始写入数据保存
-            print("开始接收文件")
-            with open(savedFileName, 'wb') as fw:
+        data1, addr1 = mainSocket.recvfrom(2048)
+        if data1 == b'START':
+            print("开始接收文件!\n")
+            with open(fileName, 'wb') as fw:
                 while True:
-                    data, addr = s.recvfrom(1024)
-                    if data == b'END':
+                    data2, addr2 = mainSocket.recvfrom(1024 * 8)
+                    if data2 == b'END':
                         print("结束接收文件")
-                        break
                     else:
-                        fw.write(data)
+                        fw.write(data2)
                         print("正在写入文件")
                 fw.close()
-       
-     # 关闭socket    
-    s.close()
 
-if __name__ == "__main__":
-    sockClient()
+    print("download done!")
+    return 0
+
+# 发送文件给服务端，上传命令后发送文件，最后接收服务端消息提示
+def uploadFile(fileName):
+     # 检测当前目录是否存在该文件
+    while not judgeFileName(fileName):
+        fileName = input("Input the exsitig file")
+
+    print("Ready to upload file to server")
+    print("upload done!")
+    return 0
+
+
+while True:
+    # 发送命令，传给服务器
+    msg = input('''Input one of the following command:\n 
+                    1. list  ----------------------------- list the files on server \n
+                    2. download [filename] ---------------download file from server\n
+                    3. upload [filename] ---------------------upload file to server \n
+                    4. exit -------------------------------------------close socket ''')
+        
+    if not msg:
+        break
+
+    # 将消息按空格进行分割，比如command[0] = upload command[1] = filename
+    command = msg.spilt()
+    
+    if command[0] == "list":
+        listFile()
+
+    elif command[0] == "download":
+        mainSocket.sendto(b'download', serverAddress)
+        downloadFile(command[1])
+       
+    elif command[0] == "upload":
+        uploadFile(command[1])
+        
+    elif command[0] == "exit":
+        mainSocket.close()
+        sys.exit()
+
+    else:
+        print("Please input the correct command.")
+      
--- Task1_CS通信/UDPServer.py
+++ Task1_CS通信/UDPServer.py
@@ -1,35 +1,40 @@
 import socket
 
+
+############################################ socket相关配置
+# 建立UDP socket
 serverAddress = ('127.0.0.1', 31500)
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(serverAddress)
-
-text_file = "data.txt"
-
+mainSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+mainSocket.bind(serverAddress)
 clientAddress = ('127.0.0.1',31501)
+
+############################################ socket相关功能函数
+
 
 while True:
     #服务器接收客户端消息
-    data, addr = s.recvfrom(2048)
+    data, addr = mainSocket.recvfrom(2048)
     if not data:
         print ("client has exist")
         break
 
     # 收到请求文件的命令 file
-    if data.decode('utf-8') == 'file':
+    if data.decode('utf-8') == 'download':
+        data1, addr1 = mainSocket.recvfrom(2048)
+        fileName = data1.decode()
         # 打开要传输的文件
-        with open(text_file, 'rb') as fs:
-            s.sendto(b'START', clientAddress)
+        with open(fileName, 'rb') as fs:
+            mainSocket.sendto(b'START', clientAddress)
             print("Start to send file")
             while True:
-                textData = fs.read(1024)
-                if not textData:
+                fileData = fs.read(1024 * 8)
+                if not fileData:
                     print("breaking from sending data")
                     break
                 else:
-                    s.sendto(textData, clientAddress)
+                    mainSocket.sendto(fileData, clientAddress)
                     print ("reading the file")
-            s.sendto(b'END', clientAddress)   
+            mainSocket.sendto(b'END', clientAddress)   
             print("End the sending file")
             fs.close()     
 
